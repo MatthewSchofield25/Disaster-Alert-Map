7ecb6a0e4105c68da595a21ff2583eaf40b88474
GailH73 <102875080+GailH73@users.noreply.github.com>
2025-04-09 16:02:15 -0500

    word cloud that works with csv data
    Currently uses test data. Need to make it functional with specific disasters. Also add more stopwords so we don't get words like "https" and individual letters.

---
+++ wc.py
@@ -0,0 +1,54 @@
+#code courtesy of Andreas Mueller and MIT
+
+import os
+import pandas as pd
+
+from os import path
+from wordcloud import WordCloud, STOPWORDS
+
+def flatten(l):
+    stack = [iter(l)]
+    result = []
+    while stack:
+        for item in stack[-1]:
+            if isinstance(item, list):
+                stack.append(iter(item))
+                break
+            result.append(item)
+        else:
+            stack.pop()
+    return result
+
+# get data directory (using getcwd() is needed to support running example in generated IPython notebook)
+#d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
+
+df = pd.read_csv('test.csv', usecols=['text'])
+text_list = df.values.tolist()
+print(text_list[:5])
+
+# Read the whole text.
+#text = open(path.join(d, 'constitution.txt')).read()
+
+text = flatten(text_list)
+text = " ".join(text)
+
+# Generate a word cloud image
+wordcloud = WordCloud().generate(text)
+
+# Display the generated image:
+# the matplotlib way:
+import matplotlib.pyplot as plt
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+
+# lower max_font_size
+wordcloud = WordCloud(max_font_size=40).generate(text)
+plt.figure()
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis("off")
+plt.show()
+
+# The pil way (if you don't have matplotlib)
+# image = wordcloud.to_image()
+# image.show()
+
