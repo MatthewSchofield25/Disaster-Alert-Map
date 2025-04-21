a192209b673d331edef510fd9fe6f60149dac380
GailH73 <102875080+GailH73@users.noreply.github.com>
2025-04-21 02:15:23 -0500

    Update AboutUsPage.py
    edited to work with new nav bar

--- AboutUsPage.py
+++ AboutUsPage.py
@@ -1,67 +1,53 @@
 import dash
 import dash_bootstrap_components.themes
 import dash_bootstrap_components as dbc
-from dash import dcc, html
+from dash import dcc, html, register_page
 import os
-
-from nltk import data
-
-#Initialize Database Connection
-driver = '{ODBC Driver 18 for SQL Server}'
-server = os.getenv("DATABASE_SERVER")
-database = os.getenv("DATABASE_NAME")
-username = os.getenv("DATABASE_USERNAME")
-password = os.getenv("DATABASE_PASSWORD")
 
 # Initialize the Dash app
 app = dash.Dash(__name__, external_stylesheets=[dash_bootstrap_components.themes.BOOTSTRAP])
 
-navbar = dbc.Navbar(
-    dbc.Container(
-        [
-            dbc.NavbarBrand("CS Project", href="#"),  # Navbar brand
-            dbc.NavbarToggler(id="navbar-toggler"),  # Navbar toggler button
-
-            dbc.Collapse(
-                dbc.Row(
-                    [
-                        # Left-aligned navigation links
-                        dbc.Col(
-                            dbc.Nav(
-                                [
-                                    dbc.NavItem(dbc.NavLink("Home", href="#", active=False)),
-                                    dbc.NavItem(dbc.NavLink("About", href="#", active=True)),
-                                    #dbc.DropdownMenu(
-                                   #     [
-                                   #         dbc.DropdownMenuItem("Action", href="#"),
-                                   #         dbc.DropdownMenuItem("Another action", href="#"),
-                                   #         dbc.DropdownMenuItem("Something else here", href="#"),
-                                    #        dbc.DropdownMenuItem(divider=True),
-                                    #        dbc.DropdownMenuItem("Separated link", href="#"),
-                                    #    ],
-                                  #      label="Dropdown",
-                                   #     nav=True,
-                                   # ),
-                                ],
-                                className="me-auto",  # Align links to the left
-                            ),
-                            width="auto",
-                        ),
-                    ],
-                    justify="between",  # Distribute space between links and search
-                    className="g-0 w-100",  # Remove gutters and take full width
-                ),
-                id="navbar-collapse",
-                is_open=False,
-                navbar=True,
-            ),
-        ],
-        fluid=True,  # Make the container fluid
-    ),
-    color="primary",
-    dark=True,
+register_page(
+    __name__,
+    "/AboutUsPage",
+    title="About Us",
+    name="About Us"
 )
 
+postofnote = html.Div(
+    className="bluesky-embed",
+    {
+        "data-bluesky-uri": "at://did:plc:k5nskatzhyxersjilvtnz4lh/app.bsky.feed.post/3ln445ahyoc2f",
+        "data-bluesky-cid": "bafyreicrmal7jdvkdj7kxxy46epzlwxzp3tfxplpjr7aw5gmnh4bwtrr7a",
+        "data-bluesky-embed-color-mode": "system"
+    },
+    children=[
+        html.P(
+            "Exclusive: Immigrants falsely labeled dead by the Social Security Administration are showing up at field offices with documents proving they are alive, leading staff to reinstate nearly three dozen people over the past week, according to records obtained by The Post.",
+            lang="en"
+        ),
+        html.Br(),
+        html.Br(),
+        html.A(
+            "[image or embed]",
+            href="https://bsky.app/profile/did:plc:k5nskatzhyxersjilvtnz4lh/post/3ln445ahyoc2f?ref_src=embed"
+        ),
+        html.P(
+            [
+                "— The Washington Post (",
+                html.A(
+                    "@washingtonpost.com",
+                    href="https://bsky.app/profile/did:plc:k5nskatzhyxersjilvtnz4lh?ref_src=embed"
+                ),
+                ") ",
+                html.A(
+                    "April 18, 2025 at 12:05 PM",
+                    href="https://bsky.app/profile/did:plc:k5nskatzhyxersjilvtnz4lh/post/3ln445ahyoc2f?ref_src=embed"
+                )
+            ]
+        )
+    ]
+)
 container_data = [
     {"label" : "Flood", "text" : "Preparation Checklist", "url" : "https://www.redcross.org/content/dam/redcross/atg/PDF_s/Preparedness___Disaster_Recovery/Disaster_Preparedness/Flood/Flood.pdf?srsltid=AfmBOooYb5XqIU4q2rpe4A2Hp28Bzj9dahoGHyrIcco_GJSxpU-J7rCo"},
     {"label" : "Drought", "text" : "Preparation", "url" : "https://www.redcross.org/get-help/how-to-prepare-for-emergencies/types-of-emergencies/drought.html#:~:text=Take%20shorter%20showers.,excess%20water%20for%20watering%20plants."},
@@ -74,40 +60,6 @@ container_data = [
     {"label" : "Hurricane", "text" : "Preparation Checklist", "url" : "https://www.redcross.org/content/dam/redcross/get-help/pdfs/hurricane/EN_Hurricane-Safety-Checklist.pdf?srsltid=AfmBOooxmu63WM4R56wSqLWsMhAy_R1khKx99umgvAz3u1aWiTwMytK9"},
     {"label" : "Tornado", "text" : "Preparation Checklist", "url" : "https://www.redcross.org/content/dam/redcross/get-help/pdfs/tornado/EN_Tornado-Safety-Checklist.pdf?srsltid=AfmBOorBQ3VJoHMyjfyCXQgXFAxITmey36YT6TFW0r4w6POiBPjM8tX7"}
 ]
-
-navbar = dbc.Navbar(
-    dbc.Container(
-        [
-            dbc.NavbarBrand("CS Project", href="#"),
-            dbc.NavbarToggler(id="navbar-toggler"),
-
-            dbc.Collapse(
-                dbc.Row(
-                    [
-                        dbc.Col(
-                            dbc.Nav(
-                                [
-                                    dbc.NavItem(dbc.NavLink("Home", href="#")),
-                                    dbc.NavItem(dbc.NavLink("About", href="#", active=True)),
-                                ],
-                                className="me-auto",
-                            ),
-                            width="auto",
-                        ),
-                    ],
-                    justify="between",
-                    className="g-0 w-100",
-                ),
-                id="navbar-collapse",
-                is_open=False,
-                navbar=True,
-            ),
-        ],
-        fluid=True,
-    ),
-    color="primary",
-    dark=True,
-)
 
 info_blurb = dbc.Container(
     dbc.Row([
@@ -123,60 +75,59 @@ info_blurb = dbc.Container(
     ])
 )
 
-# Create the app layout
-app.layout = html.Div(
-    children=[
-        navbar,
-        info_blurb,
-            dbc.Container([
-                # Heading paragraph
-                dbc.Row([
-                    dbc.Col([
-                        html.H2("External Resources", className="text-center mb-4"),
-                        html.P(
-                            "The American Red Cross provides many resources for information"
-                            "about natural disasters and the steps you can take to prepare for them."
-                            "Please see the links below to inform yourself and your loved ones in the"
-                            "case that you need to prepare for any of these disasters.",
-                            className="lead text-center mb-5"
-                        )
-                    ], width=12)
-                ]),
-
-                # Container for the information boxes
-                dbc.Row([
-                    dbc.Col([
-                        # Generate the 10 containers dynamically
-                        *[
-                            dbc.Card(
-                                dbc.CardBody([
-                                    html.H4(data["label"], className="card-title"),
-                                    html.P(
-                                        html.A(
-                                            data["text"],
-                                            href=data["url"],
-                                            target="_blank",
-                                            className="card-link"
-                                        ),
-                                        className="card-text"
-                                    )
-                                ]),
-                                className="mb-4 shadow-sm"
+layout = html.Div(
+        children=[
+            postofnote,
+            info_blurb,
+                dbc.Container([
+                    # Heading paragraph
+                    dbc.Row([
+                        dbc.Col([
+                            html.H2("External Resources", className="text-center mb-4"),
+                            html.P(
+                                "The American Red Cross provides many resources for information "
+                                "about natural disasters and the steps you can take to prepare for them. "
+                                "Please see the links below to inform yourself and your loved ones in the "
+                                "event that you need to prepare for any of these disasters.",
+                                className="lead text-center mb-5"
                             )
-                            for data in container_data
-                        ]
-                    ], width=12)
-                ])
-            ], fluid=True)
-    ]
-)
+                        ], width=12)
+                    ]),
+
+                    # Container for the information boxes
+                    dbc.Row([
+                        dbc.Col([
+                            # Generate the 10 containers dynamically
+                            *[
+                                dbc.Card(
+                                    dbc.CardBody([
+                                        html.H4(data["label"], className="card-title"),
+                                        html.P(
+                                            html.A(
+                                                data["text"],
+                                                href=data["url"],
+                                                target="_blank",
+                                                className="card-link"
+                                            ),
+                                            className="card-text"
+                                        )
+                                    ]),
+                                    className="mb-4 shadow-sm"
+                                )
+                                for data in container_data
+                            ]
+                        ], width=12)
+                    ])
+                ], fluid=True)
+        ]
+    )
 
 
-@app.callback(
-    dash.dependencies.Output("navbar-collapse", "is_open"),
-    dash.dependencies.Input("navbar-toggler", "n_clicks"),
-    dash.dependencies.State("navbar-collapse", "is_open"),
-)
+#@app.callback(
+#    dash.dependencies.Output("navbar-collapse", "is_open"),
+#    dash.dependencies.Input("navbar-toggler", "n_clicks"),
+#    dash.dependencies.State("navbar-collapse", "is_open"),
+#)
 
 def toggle_navbar_collapse(n_clicks, is_open):
     if n_clicks:
@@ -184,5 +135,5 @@ def toggle_navbar_collapse(n_clicks, is_open):
     return is_open
 
 # Run the app
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port = 8050)
+#if __name__ == '__main__':
+#    app.run(debug=True, host='127.0.0.1', port = 8050)
