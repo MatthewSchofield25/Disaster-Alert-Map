210595213278dddb380db630c7331a33cfe82584
GailH73 <102875080+GailH73@users.noreply.github.com>
2025-05-02 23:11:08 -0500

    refined word cloud

--- wc.py
+++ wc.py
@@ -1,8 +1,7 @@
-#code courtesy of Andreas Mueller and MIT
+#word cloud library courtesy of Andreas Mueller and MIT
 
 import os
 import pandas as pd
-
 from os import path
 from wordcloud import WordCloud, STOPWORDS
 
@@ -30,18 +29,19 @@ text_list = df.values.tolist()
 
 text = flatten(text_list)
 text = " ".join(text)
-
+more_stopwords = {"t", "co", "http", "https", "oh", "ÛÒ", "Û", "Û_", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "shit", "fuck", "ass", "rt", "amp", "via", "get", "let"}
+for item in more_stopwords:
+    STOPWORDS.add(item)
 # Generate a word cloud image
 wordcloud = WordCloud().generate(text)
 
 # Display the generated image:
 # the matplotlib way:
 import matplotlib.pyplot as plt
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
+import matplotlib.cm as cm
 
 # lower max_font_size
-wordcloud = WordCloud(max_font_size=40).generate(text)
+wordcloud = WordCloud(max_font_size=40, min_font_size=10, colormap= cm.cool , min_word_length=3).generate(text)
 plt.figure()
 plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
@@ -50,4 +50,3 @@ plt.show()
 # The pil way (if you don't have matplotlib)
 # image = wordcloud.to_image()
 # image.show()
-
